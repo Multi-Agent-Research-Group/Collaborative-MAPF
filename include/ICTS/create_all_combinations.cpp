0804af1cc26cdb74f538ae665b57531853760f59
kushal2000 <kushal.k.2000@gmail.com>
2021-04-11 00:15:37 +0530

    Create all combinations of slack code added

---
+++ include/ICTS/create_all_combinations.cpp
@@ -0,0 +1,190 @@
+  #include <iostream>                  // for std::cout
+  #include <utility>                   // for std::pair
+  #include <algorithm>                 // for std::for_each
+  #include <boost/graph/graph_traits.hpp>
+  #include <boost/graph/adjacency_list.hpp>
+  #include <boost/graph/transpose_graph.hpp>
+  #include <boost/graph/topological_sort.hpp>
+  #include <bits/stdc++.h>
+  using namespace boost;
+  // using namespace std;
+
+struct meta_data_t {
+  typedef vertex_property_tag kind;
+};
+
+struct meta_data{
+  std::pair <int, int> start;
+  std::pair <int, int> goal;
+
+  std::vector <int> agent_list;
+
+  int  start_time;
+  int end_time;
+
+  int slack = 0;
+  public:
+    bool compare(const meta_data & param)
+    {
+      //...compare structs here like:
+      if (start != param.start)
+        return false;
+        //write something
+
+      //etc...
+
+      //at the end you can also return bool that indicates that structs are equal or not
+      return true;
+    }
+};
+
+typedef property<meta_data_t, meta_data> MetaData;
+typedef adjacency_list<vecS, vecS, directedS, 
+                       MetaData> PrecedenceConstraintGraph;
+
+typedef std::pair<int,int> Pair;
+
+typedef boost::graph_traits<PrecedenceConstraintGraph>::vertex_descriptor Vertex;
+typedef std::vector< Vertex > container;
+/// Boost graph out edge iterator
+typedef boost::graph_traits<PrecedenceConstraintGraph>::out_edge_iterator OutEdgeIter;
+
+
+void print_topological(PrecedenceConstraintGraph &G){
+  container c;
+  topological_sort(G, std::back_inserter(c));
+  property_map<PrecedenceConstraintGraph, meta_data_t>::type name = get(meta_data_t(), G);
+  std::cout << "A topological ordering: ";
+  for ( container::reverse_iterator ii=c.rbegin(); ii!=c.rend(); ++ii)
+  {
+    std::cout << std::endl;
+    meta_data vertex = get(name, *ii);
+    std::cout << vertex.start_time << " ";
+    std::cout << vertex.end_time << " ";   
+    for(auto agent: vertex.agent_list){
+      std::cout << agent << " ";   
+    }
+  }
+  std::cout <<std::endl;
+}
+
+bool checkpathpossible(PrecedenceConstraintGraph &G){
+  container c;
+  topological_sort(G, std::back_inserter(c));
+  property_map<PrecedenceConstraintGraph, meta_data_t>::type name = get(meta_data_t(), G);
+  std::cout << "A topological ordering: ";
+  for ( container::reverse_iterator ii=c.rbegin(); ii!=c.rend(); ++ii)
+  {
+    std::cout << std::endl;
+    meta_data vertex = get(name, *ii);
+    std::cout << vertex.start_time << " ";
+    std::cout << vertex.end_time << " ";   
+    for(auto agent: vertex.agent_list){
+      std::cout << agent << " ";   
+    }
+    std::cout << vertex.slack << " ";   
+    // cout << boost::index(*ii) << std::endl;
+  }
+  std::cout <<std::endl;
+  return false;
+}
+
+bool generatePaths(PrecedenceConstraintGraph &G, PrecedenceConstraintGraph &G_T, container &c, container::iterator ii){
+  property_map<PrecedenceConstraintGraph, meta_data_t>::type name = get(meta_data_t(), G);
+  property_map<PrecedenceConstraintGraph, meta_data_t>::type name_t = get(meta_data_t(), G_T);
+    
+  if(c.rbegin().base()-1 == ii){
+    return checkpathpossible(G);
+  }
+
+  container predecessors;
+  OutEdgeIter ei, ei_end;
+  for (boost::tie(ei, ei_end) = out_edges(*ii, G_T); ei != ei_end; ++ei) 
+  {
+      Vertex curPred = target(*ei, G_T);
+      predecessors.push_back(curPred);
+  }
+
+  container::iterator jj = ii;
+  jj++;
+
+  if(predecessors.size() == 0){
+    return checkpathpossible(G) || generatePaths(G, G_T, c, jj);
+  }
+
+  meta_data *curr_vertex = &get(name_t, *ii); 
+
+  
+  if(generatePaths(G, G_T, c, jj))
+    return true;
+  
+  int slack = curr_vertex->slack;
+
+  while(curr_vertex->slack){
+      curr_vertex->slack--;
+      
+      for(auto pred:predecessors){
+        meta_data *vertex = &get(name, pred);
+        vertex->slack+=1;
+      }
+      if(generatePaths(G, G_T, c, jj))
+        return true;
+  }
+  curr_vertex->slack = slack;
+  for(auto pred:predecessors){
+    meta_data *vertex = &get(name, pred);
+    vertex->slack+=1;
+  }
+  return false;
+}
+
+int main()
+{
+  Pair edge_array[11] = { Pair(0,1), Pair(1,2), Pair(2,5), 
+                          Pair(3,5), Pair(4,6), Pair(5,7), 
+                          Pair(5,8), Pair(6,9), Pair(7,10), 
+                          Pair(8,11), Pair(9,11) };
+    
+  PrecedenceConstraintGraph G(12);
+  PrecedenceConstraintGraph invG(12);
+
+  property_map<PrecedenceConstraintGraph, meta_data_t>::type name = get(meta_data_t(), G);
+    
+  meta_data random = {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {0, 1}, 0, 5, 10,};
+
+  name[0] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {0}, 0, 5, 0,};
+  name[1] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {0}, 6, 15, 0,};
+  name[2] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {0}, 16, 20, 0,};
+  name[3] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {1}, 0, 10, 10,};
+  name[4] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {2}, 0, 10, 0,};
+  name[5] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {0, 1}, 21, 30, 0,};
+  name[6] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {2}, 10, 15, 0,};
+  name[7] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {0}, 31, 40, 0,};
+  name[8] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {1}, 31, 35, 0,};
+  name[9] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {2}, 16, 25, 10,};
+  name[10] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {0, 1}, 41, 50, 0,};
+  name[11] = meta_data {std::make_pair(1, 2), std::make_pair(1, 2), std::vector <int> {0, 1}, 36, 45, 5,};
+
+  for (int i = 0; i < 11; ++i)
+    add_edge(edge_array[i].first, edge_array[i].second, G);
+
+  container c;
+  topological_sort(G, std::back_inserter(c));
+
+  PrecedenceConstraintGraph G_T;
+  transpose_graph(G, G_T);
+
+  // print_graph(G_T, name_map);
+  // std::unordered_map <container::iterator ii, container::iterator ii> parents;
+  // typedef boost::graph_traits<Graph>::vertex_iterator VertexIter;
+
+  // VertexIter vi, vend;
+  // for (boost::tie(vi, vi_end) = boost::vertices(graph); vi != vi_end; ++vi)
+  // {
+  // }
+  // print_topological(G_T);
+  generatePaths(G, G_T, c, c.begin());
+  // print_topological(G);
+}
+
+
