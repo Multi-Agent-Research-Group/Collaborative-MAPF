f7085f58c50155e167c124a5e72ba1cda813d2f5
kushal2000 <kushal.k.2000@gmail.com>
2021-04-30 01:19:10 +0530

    Added interface for reading PC graphs from .txt files with updated image and graph files

--- examples/create_problems.py
+++ examples/create_problems.py
@@ -2,10 +2,19 @@ import numpy as np
 from PIL import Image as im
 
 
-env_map = np.ones((300, 300))
+env_map = np.ones((300, 300))*255
 
-env_map[0,0]=0
 
-env_img = im.fromarray(env_map)
 
-env_img.save('env.png')
+
+# 60
+# 84
+
+
+# 45
+env_map[10:(300-30),55:65]=0
+env_map[10:(300-30),80:90]=0
+# Image.fromarray(b)
+env_img = im.fromarray((env_map).astype(np.uint8))
+
+env_img.save('env_obstacles.png')
