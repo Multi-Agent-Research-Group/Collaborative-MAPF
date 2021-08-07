510c6b3f722ac2af264fc3e7ce8f30702a2bee41
Kushal Kedia <kushal.k.2000@gmail.com>
2021-08-07 16:00:32 +0530

    Cache problem sets

--- include/HCBS/CBS.hpp
+++ include/HCBS/CBS.hpp
@@ -414,7 +414,7 @@ public:
 
 			auto stop = high_resolution_clock::now();
 			std::chrono::duration<double, std::micro> timespent = stop - solve_start_time;
-			if (timespent.count() > 100000000)
+			if (timespent.count() > 300000000)
 			{
 				break;
 			}
