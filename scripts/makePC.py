5f791a50de926fbe1b9f7838ec1c45fc65928da8
Kushal Kedia <kushal.k.2000@gmail.com>
2021-11-17 02:10:31 +0530

    Add problem sets for ablation studies

--- scripts/makePC.py
+++ scripts/makePC.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 
-agents = [2]
-tasks = [2, 3, 4]
+agents = [2,4,6,8,10]
+tasks = [2]
 # agents = [3]
 # tasks = [2]
 degrees = [1]
@@ -15,7 +15,7 @@ for agent in agents:
 		for o in obstacle_files:
 			o_file = f'/home/kushal/ros_ws/src/CMAPF/data/obstacles/{o}'
 
-			p_file = f'/home/kushal/ros_ws/src/CMAPF/data/PC/{o}/{agent}_{task}_1/'
+			p_file = f'/home/kushal/ros_ws/src/CMAPF/data/PC_New/{o}/{agent}_{task}_1/'
 
 			Path(p_file+'/CBS').mkdir(exist_ok = True, parents=True)
 			Path(p_file+'/ICTS').mkdir(exist_ok = True, parents=True)
